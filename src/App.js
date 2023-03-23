0d9d56d74d9807b620a75e3aebf7df9329690751
Olena <alenaelena13.01@gmail.com>
2023-03-23 15:51:51 +0200

    moved scss to dir scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header/Header';
 import Intro from './components/Intro';
-import Products from './components/products/Products'
+import Products from './components/products/Products';
+import CartModal from './components/CartModal'
 
 function App() {
   return (
     <div className='wrap'>
+      <CartModal/>
       <Header />
       <Intro />
       <Products/>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './shop';
-import './style.scss';
+import './scss/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
